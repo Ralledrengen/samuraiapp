6e5634e70b2a1cc61d322d2869115935ecfd185c
SKAB6-PC-07\Tec <epicmodding649@gmail.com>
2020-09-08 08:56:39 +0200

    Got through tut3 - moving on to tut4

--- samuraiapp.Data/SamuraiContext.cs
+++ samuraiapp.Data/SamuraiContext.cs
@@ -10,9 +10,9 @@ namespace SamuraiApp.Data
     public class SamuraiContext:DbContext
     {
         //Nu til 3
-        public DbSet<Samurai> Samurais { get; set; }
-        public DbSet<Quote> Quotes { get; set; }
-        public DbSet<Clan> Clans { get; set; }
+        public virtual DbSet<Samurai> Samurais { get; set; }
+        public virtual DbSet<Quote> Quotes { get; set; }
+        public virtual DbSet<Clan> Clans { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
